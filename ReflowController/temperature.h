dc2e504ccad9c56ac16d2ae1161b342a6399199d
David Sanz Kirbis <info@dasaki.com>
2017-05-29 14:29:25 +0200

    Initial commit after fork & heavy reestructuration
    All has been heavily debugged and reestructured offline. This is the first upload of these changes.

--- ReflowController/temperature.h
+++ ReflowController/temperature.h
@@ -3,7 +3,10 @@
 
 #include <Arduino.h>
 #include <SPI.h>
+#include "config.h"
 
+
+#ifdef SENSOR_MAX31855
 typedef union {
   uint32_t value;
   uint8_t bytes[4];
@@ -43,21 +46,39 @@ typedef union {
   };
 } __attribute__((packed)) MAX31855_t;
 
+#else ifdef SENSOR_MAX6675
+
+#define MIN_VALID_TEMP_MAX6675 10
+#define MAX_VALID_TEMP_MAX6675 500
+
+typedef union {
+  uint16_t value;
+  uint8_t bytes[2];
+} __attribute__((packed)) MAX6675_t;
+
+#endif
+
+
 typedef struct Thermocouple {
   double temperature;
   uint8_t stat;
   uint8_t chipSelect;
 };
 
-#define LCD_CS 10
+
+
 
 void readThermocouple(struct Thermocouple* input) {
-  MAX31855_t sensor;
+  
 
   uint8_t lcdState = digitalRead(LCD_CS);
   digitalWrite(LCD_CS, HIGH);
   digitalWrite(input->chipSelect, LOW);
   delay(1);
+
+  
+#ifdef SENSOR_MAX31855
+  MAX31855_t sensor;
   
   for (int8_t i = 3; i >= 0; i--) {
     sensor.bytes[i] = SPI.transfer(0x00);
@@ -68,8 +89,32 @@ void readThermocouple(struct Thermocouple* input) {
   uint16_t value = (sensor.value >> 18) & 0x3FFF; // mask off the sign bit and shit to the correct alignment for the temp data  
   input->temperature = value * 0.25;
 
-  digitalWrite(input->chipSelect, HIGH);
+#else ifdef SENSOR_MAX6675
+  MAX6675_t sensor;
+  sensor.bytes[3] = 0;
+  sensor.bytes[2] = 0;
+  sensor.bytes[1] = SPI.transfer(0x00);
+  sensor.bytes[0] = SPI.transfer(0x00);
+  if (sensor.value & 0x4) {}
+  else {
+    sensor.value >>= 3;
+    double temp = sensor.value *0.25;
+    // got wrong readings of the MAX6675 with value 1
+    if ((temp > MIN_VALID_TEMP_MAX6675) && (temp < MAX_VALID_TEMP_MAX6675)) {
+      input->temperature = temp;
+    }
+  }
+
+#endif
+
+  digitalWrite(input->chipSelect, HIGH); 
+
   digitalWrite(LCD_CS, lcdState);
+  
+  
+  
+ 
 }
+
 
 #endif
